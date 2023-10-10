0389aad702007e441cd44da4a49c1b1003a8a854
WendyChen9 <31325518+WendyChen9@users.noreply.github.com>
2023-10-09 16:41:17 -1000

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 function tableFinder() {
 
-    var lastName = document.getElementById("floatingInput").value;
+    var lastName = document.getElementById("floatingInput").value.toLowerCase();
 
-    const t3 = ["3","Alex Akaka", "Bella Weiss", "Kellen Chow", "Matthew Matsunaga", "Elena Rosenberg-Carlson"];
-    const t4 = ["4", "Michael Miller", "Greg Madrey-Miller", "Amy Ku", "Eric Bicocchi", "May Salcedo", "Bryson Monma"];
-    const t5 = ["5","Igor Katsyv", "Joy Guo", "Carleigh Canterbury", "Austin Shackelford", "Kelsey Uyeda", "Chris Uyeda"]
-
+    const t3 = ["3","Alex Akaka", "Bella Weiss", "Kellen Chow", "Matthew Matsunaga", "Elena Rosenberg-Carlson"].map(v => v.toLowerCase());
+    const t4 = ["4", "Michael Miller", "Greg Madrey-Miller", "Amy Ku", "Eric Bicocchi", "May Salcedo", "Bryson Monma"].map(v => v.toLowerCase());
+    const t5 = ["5","Igor Katsyv", "Joy Guo", "Carleigh Canterbury", "Austin Shackelford", "Kelsey Uyeda", "Chris Uyeda"].map(v => v.toLowerCase());
+    const t6 = ["6", "Wen-Dai Lin", "Christian Simifranca", "Megan Marler", "Melvin Lin", "Dean Lin", "Sean Lin"].map(v => v.toLowerCase());
 
     var tables = [t3, t4, t5];
     var num;
